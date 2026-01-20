9703cf9a0323f3493094b5c7600bd042c993467d
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-01-20 13:30:16 +0100

    Chapitre 3 loops for and while and foreach

---
+++ CHAPITRES/CHAPITRE3/loops.php
@@ -0,0 +1,28 @@
+<?php
+
+$numDays = 7;
+
+for ($i=1; $i <= $numDays ; $i++) { 
+    echo "Day: ". $i ."<br>";
+}
+
+/*========================*/echo '<br>';/*========================*/
+
+$numMonthes = 12;
+
+$index = 1;
+
+while ($index <= $numMonthes) { 
+    echo "Month: ". $index. "<br>";
+    $index++;
+    }
+    
+    
+/*========================*/echo '<br>';/*========================*/
+
+$family = array("grandamather","grandfather","brother","uncle","aunt");
+
+
+foreach ($family as $fam) {
+    echo "i have ". $fam ." and ";
+}
