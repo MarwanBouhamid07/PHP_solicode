56536e5cf6f1f596b06607f5e525b09d88115fdb
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-01-20 23:26:51 +0100

    Chapitre 4 array

---
+++ CHAPITRES/CHAPITRE4/array.php
@@ -0,0 +1,23 @@
+<?php
+$menu = array("coffe","juic","milk","tea","caposheno");
+
+echo "Hi how are you do you want to drink $menu[0] if you don't want it ";
+array_push($menu,"cooca");
+
+unset($menu[5]);
+
+echo 'this is our menu: <br>';
+foreach($menu as $menu){
+echo "<b>$menu</b> <br>";
+};
+
+
+$prixMenu = array(
+    "coffe" => "10$",
+    "juic" => "12$",
+    "milk" => "9$",
+    "tea" => "5$",
+    "caposheno" => "25$",
+) ;
+
+echo "prix of ". $menu[4] ." is ". $prixMenu["juic"];
